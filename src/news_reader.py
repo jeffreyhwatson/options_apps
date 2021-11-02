255f3b0db6a6dff10877fe3f9afac2cddba507cd
Hanif Watson <jeffrey.h.watson@protonmail.com>
2021-11-02 14:29:25 -0700

    Initial Commit

---
+++ src/news_reader.py
@@ -0,0 +1,9 @@
+import requests, math
+from bs4 import BeautifulSoup
+import spacy
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from datetime import datetime
+import time
