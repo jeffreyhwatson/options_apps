0ae77f4ce42f9b843f2a0017ec789af694159a89
Hanif Watson <jeffrey.h.watson@protonmail.com>
2021-11-05 22:07:40 -0700

    Add price() helper function

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -8,3 +8,36 @@ import seaborn as sns
 from datetime import datetime
 import time
 
+def prices(symbol: str):
+    "Retuns a data frame of stock price information."
+    
+    # creating empty stock info dictionary
+    stock_data = {
+    'Company': [],
+    'Symbol': [],
+    'currentPrice': [],
+    'dayHigh': [],
+    'dayLow': [],
+    '52wkHigh': [],
+    '52wkLow': [],
+    'dividendRate': []
+    }
+    # appending data from yf
+    stock_info = yf.Ticker(symbol).info
+    stock_data['Company'].append(stock_info['shortName'])
+    stock_data['Symbol'].append(stock_info['symbol'])
+    stock_data['currentPrice'].append(stock_info['currentPrice'])
+    stock_data['dayHigh'].append(stock_info['dayHigh'])
+    stock_data['dayLow'].append(stock_info['dayLow'])
+    stock_data['52wkHigh'].append(stock_info['fiftyTwoWeekHigh'])
+    stock_data['52wkLow'].append(stock_info['fiftyTwoWeekLow'])            
+            
+    # converting dividend None types to floats
+    dividend = stock_info['dividendRate']
+    if dividend != None:
+        dividend = dividend
+    else:
+        dividend = 0
+    stock_data['dividendRate'].append(dividend)
+    
+    return pd.DataFrame(stock_data)
